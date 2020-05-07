77c3799bc7ba44cbeaf9ba5ae7c692d6c0e90a48
Bijay Shrestha <bzaystha111@gmail.com>
2020-05-07 16:37:00 +1000

    Product Api updated

--- WebApi/Controllers/ProductApiController.cs
+++ WebApi/Controllers/ProductApiController.cs
@@ -66,7 +66,6 @@ namespace WebApi.Controllers
 
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutTodoItem(int id, ProductDetail todoItem)
         {
